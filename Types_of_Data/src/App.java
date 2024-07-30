b8445d0371464dc97fdee23aa13c332a09346c31
ertaser <106701345+ertaser@users.noreply.github.com>
2024-07-30 13:47:25 +0500

    Add files via upload

---
+++ Types_of_Data/src/App.java
@@ -0,0 +1,11 @@
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+    
+        double doub = Math.random();
+        byte by = 127;
+
+        System.out.println(" "+doub+" "+by);
+    }
+}
